e7a1425b8681ad544208df7ed4a83c8cb3067f69
Lucas Torres <lucastorreslr10@gmail.com>
2025-04-28 16:35:26 -0300

    LL insert-node

---
+++ Linked Lists/insert-node.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+  public:
+    int value;
+    Node* next;
+
+  Node(int value){
+    this->value = value;
+    this->next = nullptr;
+  }
+};
+
+class LinkedList{
+  private:
+    Node* head;
+    Node* tail;
+    int length;
+
+  public:
+    LinkedList(int value){
+      Node* newNode = new Node(value);
+      head = newNode;
+      tail = newNode;
+      length = 1;
+    }
+
+    ~LinkedList(){
+      Node* temp = head;
+      while(head){
+        head = head->next;
+        delete temp;
+        temp = head;
+      }
+    }
+
+    void printList(){
+      Node* temp = head;
+
+      while(temp){
+        cout << temp->value << " ";
+        temp = temp->next;
+      }
+    }
+
+    void appendNode(int value){
+      Node* newNode = new Node(value);
+
+      if(length == 0){
+        this->head = newNode;
+        this->tail = newNode;
+      } 
+      else {
+        tail->next = newNode;
+        this->tail = newNode;
+      }
+      length++;
+    }
+
+    void prependNode(int value){
+      Node* newNode = new Node(value);
+
+      if(length == 0){
+        head = newNode;
+        tail = newNode;
+      } else{
+        newNode->next = head;
+        head = newNode;
+      }
+      length++;
+    }
+
+    bool insertNode(int index, int value){
+      if (index < 0 || index > length) return false;
+
+      if (index == 0){
+        prependNode(value);
+        return true;
+      }
+
+      if (index == length){
+        appendNode(value);
+        return true;
+      }
+
+      Node* newNode = new Node(value);
+      Node* temp = getNode(index - 1);
+
+      newNode->next = temp->next;
+      temp->next = newNode;
+      length++;
+      return true;
+    }
+
+    void deleteFirst(){
+      if(length == 0) return;
+      Node* temp = head;
+
+      if(length == 1){
+        head = nullptr;
+        tail = nullptr;
+      } else{
+        head = head->next;
+      }
+
+      length--;
+      delete temp;
+    }
+
+    void deleteLast(){
+      if(length == 0) return;
+      Node* temp = head;
+
+      if (length == 1){
+        head = nullptr;
+        tail = nullptr;
+      } else {
+        Node* pre = head;
+
+        while(temp->next){
+          pre = temp;
+          temp = temp->next;
+        }
+        
+        tail = pre;
+        tail->next = nullptr;
+      }
+
+      length--;
+      delete temp;
+    }
+
+    Node* getNode(int index){
+      if (index < 0 || index >= length){
+        return nullptr;
+      }
+      Node* temp = head;
+
+      for(int i = 0; i < index; i++){
+        temp = temp->next;
+      }
+      return temp;
+    }
+
+    bool setNode(int index, int value){
+      Node* temp = getNode(index);
+
+      if (temp){
+        temp->value = value;
+        return true;
+      } else{
+        return false;
+      }
+    }
+
+    void getHead(){
+      cout << "Head: " << head->value << endl;
+    }
+
+    void getTail(){
+      cout << "Tail: " << tail->value << endl;
+    }
+
+    void getLength(){
+      cout << "Length: " << length << endl;
+    }
+};
+
+int main(){
+  LinkedList* myLinkedList = new LinkedList(0);
+  myLinkedList->appendNode(2);
+
+  myLinkedList->insertNode(1,1);
+  myLinkedList->printList();
+}
